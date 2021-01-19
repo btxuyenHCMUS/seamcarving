90dafecff97ef12e927e382b19274daedc2008be
ndh98 <ndh98@users.noreply.github.com>
2021-01-19 18:19:25 +0700

    Update findSeamParallel.cu
    located the seam

--- findSeamParallel.cu
+++ findSeamParallel.cu
@@ -91,6 +91,44 @@ void readPnm(char * fileName,
 	fclose(f);
 }
 
+void readPnmUchar3(char * fileName,
+		int &width, int &height, uchar3 * &pixels)
+{
+	FILE * f = fopen(fileName, "r");
+	if (f == NULL)
+	{
+		printf("Cannot read %s\n", fileName);
+		exit(EXIT_FAILURE);
+	}
+
+	char type[3];
+	fscanf(f, "%s", type);
+
+	if (strcmp(type, "P3") != 0) // In this exercise, we don't touch other types
+	{
+		fclose(f);
+		printf("Cannot read %s\n", fileName);
+		exit(EXIT_FAILURE);
+	}
+
+	fscanf(f, "%i", &width);
+	fscanf(f, "%i", &height);
+
+	int max_val;
+	fscanf(f, "%i", &max_val);
+	if (max_val > 255) // In this exercise, we assume 1 byte per value
+	{
+		fclose(f);
+		printf("Cannot read %s\n", fileName);
+		exit(EXIT_FAILURE);
+	}
+
+	pixels = (uchar3 *)malloc(width * height * sizeof(uchar3));
+	for (int i = 0; i < width * height; i++)
+		fscanf(f, "%hhu%hhu%hhu", &pixels[i].x, &pixels[i].y, &pixels[i].z);
+
+	fclose(f);
+}
 void writePnm(uint8_t * pixels, int numChannels, int width, int height,
 		char * fileName)
 {
@@ -119,41 +157,62 @@ void writePnm(uint8_t * pixels, int numChannels, int width, int height,
 
 	fclose(f);
 }
+void writePnmUchar3(uchar3 * pixels, int width, int height,
+		char * fileName)
+{
+	FILE * f = fopen(fileName, "w");
+	if (f == NULL)
+	{
+		printf("Cannot write %s\n", fileName);
+		exit(EXIT_FAILURE);
+	}
 
-__global__ void findSeamKernel(uint8_t* inPixels, int width, int height,		
-		uint8_t* outPixels)
+	fprintf(f, "P3\n%i\n%i\n255\n", width, height);
+
+	for (int i = 0; i < width * height; i++)
+		fprintf(f, "%hhu\n%hhu\n%hhu\n", pixels[i].x, pixels[i].y, pixels[i].z);
+
+	fclose(f);
+}
+
+__global__ void findSeamKernel(uint8_t* inEnergy, int width, int height,
+		uint8_t* outMap, int *line)
 {
 	// TODO
-
+  int rowIndex = height - 1;
   int c = threadIdx.x + blockIdx.x * blockDim.x;
-	int r = threadIdx.y + blockIdx.y * blockDim.y;// chỉ số phần tử pixle
-  if (r < height && c < width) //check có cần làm hay không, thread ngoài biên thì không làm
+  if (c < width)
   {
-    int i = r * width + c;
-    float outx = 0.0f;
-    float outy = 0.0f;
-    int iFilter = 0; //chỉ số phần tử ở filter
-    for (int x = -1; x <= 1; x++)
-      for (int y = -1; y <= 1; y++) //duyệt filter
-      {
-        int rx = r + x;
-        int cy = c + y;
-        if (rx < 0) rx = 0;
-        if (rx > height - 1) rx = height - 1;
-        if (cy < 0) cy = 0;
-        if (cy > width - 1) cy = width - 1; // ngoài biên thì lấy phần tử gần nhất
-        int k = rx * width + cy;
-        outx += float(float(inPixels[k]) * float(filterX[iFilter]/4));
-        outy += float(float(inPixels[k]) * float(filterY[iFilter]/4));
-
-        iFilter++;
-      }
-    outPixels[i] = abs(outx) + abs(outy);
+    outMap[rowIndex * width + c] = inEnergy[rowIndex * width + c];
+    __syncthreads();
+    int val1 = 255;
+    int val2 = 255;
+    int val3 = 255;
+    int minVal;
+    while (rowIndex>0)
+    {
+      rowIndex--;
+      if (c>0)
+        val1 = outMap[(rowIndex + 1) * width + c - 1];
+      else val1 = 255;
+      val2 = outMap[(rowIndex + 1) * width + c];
+      if (c < width - 1)
+        val3 = outMap[(rowIndex + 1) * width + c + 1];
+      else val3 = 255;
+      minVal = min(val1, min(val2, val3));
+      outMap[rowIndex * width + c] = minVal + inEnergy[rowIndex * width + c];
+      if (minVal == val1)
+        line[rowIndex * width + c] = -1;
+      else if (minVal == val2)
+        line[rowIndex *  width + c] = 0;
+      else line[rowIndex * width + c] = 1;
+      __syncthreads();
+    }
   }
 }
 
-void detectionEdge(uint8_t * inPixels, int width, int height, float *filterX, float *filterY, int filterWidth,
-		uint8_t * outPixels, dim3 blockSize=dim3(1, 1))
+void findSeam(uint8_t * inEnergy, int width, int height,
+    uint8_t * outMap, int* line, dim3 blockSize=dim3(1, 1))
 {
 	GpuTimer timer;
 	timer.Start();
@@ -164,19 +223,16 @@ void detectionEdge(uint8_t * inPixels, int width, int height, float *filterX, fl
 
 	// TODO
   //Host allocates memories on device
-  uint8_t *d_inPixels, *d_outPixels;
-  float *d_filterX, *d_filterY;
-  CHECK(cudaMalloc(&d_inPixels, width * height * sizeof(uint8_t)));
-  CHECK(cudaMalloc(&d_outPixels, width * height * sizeof(uint8_t)));
-  CHECK(cudaMalloc(&d_filterX, filterWidth * filterWidth * sizeof(float)));
-  CHECK(cudaMalloc(&d_filterY, filterWidth * filterWidth * sizeof(float)));
+  int *d_line;
+  uint8_t *d_inEnergy, *d_outMap;
+  CHECK(cudaMalloc(&d_inEnergy, width * height * sizeof(uint8_t)));
+  CHECK(cudaMalloc(&d_outMap, width * height * sizeof(uint8_t)));
+  CHECK(cudaMalloc(&d_line, width * height * sizeof(int)));
   //Host copies data to divece memories
-  CHECK(cudaMemcpy(d_inPixels, inPixels, width * height * sizeof(uint8_t), cudaMemcpyHostToDevice));
-  CHECK(cudaMemcpy(d_filterX, filterX, filterWidth * filterWidth * sizeof(float), cudaMemcpyHostToDevice));
-  CHECK(cudaMemcpy(d_filterY, filterY, filterWidth * filterWidth * sizeof(float), cudaMemcpyHostToDevice));
+  CHECK(cudaMemcpy(d_inEnergy, inEnergy, width * height * sizeof(uint8_t), cudaMemcpyHostToDevice));
   //Host invokes kernel function
   dim3 gridSize((width - 1)/blockSize.x + 1, (height - 1)/blockSize.y + 1);
-  edgeDetectionKernel<<<gridSize, blockSize>>>(d_inPixels, width, height, d_filterX, d_filterY, filterWidth, d_outPixels);
+  findSeamKernel<<<gridSize, blockSize>>>(d_inEnergy, width, height, d_outMap, d_line);
   cudaError_t errSync  = cudaGetLastError();
 	cudaError_t errAsync = cudaDeviceSynchronize();
 	if (errSync != cudaSuccess)
@@ -184,52 +240,72 @@ void detectionEdge(uint8_t * inPixels, int width, int height, float *filterX, fl
 	if (errAsync != cudaSuccess)
 	  printf("Async kernel error: %s\n", cudaGetErrorString(errAsync));
   //Host copies result form divece memories
-  CHECK(cudaMemcpy(outPixels, d_outPixels, width * height * sizeof(uint8_t), cudaMemcpyDeviceToHost));
+  CHECK(cudaMemcpy(outMap, d_outMap, width * height * sizeof(uint8_t), cudaMemcpyDeviceToHost));
+  CHECK(cudaMemcpy(line, d_line, width * height * sizeof(int), cudaMemcpyDeviceToHost));
   //Host free device memories
-  CHECK(cudaFree(d_inPixels));
-  CHECK(cudaFree(d_outPixels));
-  CHECK(cudaFree(d_filterX));
-  CHECK(cudaFree(d_filterY));
+  CHECK(cudaFree(d_inEnergy));
+  CHECK(cudaFree(d_outMap));
+  CHECK(cudaFree(d_line));
 	timer.Stop();
 	float time = timer.Elapsed();
-	printf("Processing time %f ms use device", time);
+	printf("Processing time %f ms use device\n", time);
+}
+void checkSeam(uint8_t *map, uchar3 *pixels, int width, int height, int *line)
+{
+  int k = 0;
+  for (int c = 0; c < width; c++)
+    {
+      if (map[c]<=map[k])
+      {
+        k = c;
+      }
+      printf("%i", map[c]);
+    }
+  pixels[k].x = 255;
+  pixels[k].y = 0;
+  pixels[k].z = 0;
+  int r = 0;
+  printf("%i %i\n", r, k);
+  while (r < height)
+  {
+    r++;
+    k +=  line[(r-1)*width+k];
+    pixels[r*width+k].x = 255;
+    pixels[r*width+k].y = 0;
+    pixels[r*width+k].z = 0;
+    printf("%i %i\n", r, k);
+  }
 }
 
 int main(int argc, char ** argv)
 {
-	if (argc != 3 && argc != 5)
-	{
-		printf("The number of arguments is invalid\n");
-		return EXIT_FAILURE;
-	}
-
 	// Read input image file
 	int numChannels, width, height;
-	uint8_t * inPixels;
-	readPnm(argv[1], numChannels, width, height, inPixels);
+  uchar3 * inPixels;
+  uint8_t *gray;
+
+	readPnm(argv[1], numChannels, width, height, gray);
+  readPnmUchar3(argv[2], width, height, inPixels);
 	printf("Image size (width x height): %i x %i\n\n", width, height);
 
-	// Set up filter xSobel
-	float filterX [9]= {1, 0, -1, 2, 0, -2, 1, 0, -1};
-  // Set up filter ySobel
-	float filterY [9]= {1, 2, 1, 0, 0, 0, -1, -2, -1};
-
-	// Blur input image using device
-	uint8_t * deviceOutPixels = (uint8_t *)malloc(width * height * sizeof(uint8_t));
-	dim3 blockSize(32, 32); // Default
+		// Blur input image using device
+	uint8_t * map = (uint8_t *)malloc(width * height * sizeof(uint8_t));
+  int *line = (int*)malloc(width * height * sizeof(int));
+	dim3 blockSize(512, 512); // Default
 	if (argc == 6)
 	{
 		blockSize.x = atoi(argv[4]);
 		blockSize.y = atoi(argv[5]);
 	}
-	detectionEdge(inPixels, width, height, filterX, filterY, 3, deviceOutPixels, blockSize);
-
+	findSeam(gray, width, height, map, line, 512);
+  checkSeam(map, inPixels, width, height, line);
 	// Write results to files
-	char * name = argv[2];
+	char * name = argv[3];
   //char * nameedge = "edge.txt";
-	writePnm(deviceOutPixels, 1, width, height, name);
+	writePnmUchar3(inPixels, width, height, name);
   //writeMatrix(deviceOutPixels, 1, width, height, name);
 	// Free memories
 	free(inPixels);
-	free(deviceOutPixels);
+	free(map);
+  free(line);
 }
